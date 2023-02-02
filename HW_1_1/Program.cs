4998d7a478b5e2424cf7a9fdbbeed8508c89d959
suchakree-petn <suchakree.petn@kmutt.ac.th>
2023-02-02 19:11:26 +0700

    Add HW_1_1

---
+++ HW_1_1/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        double n = double.Parse(Console.ReadLine());
+        double ans = condition(n);
+        if (ans == -999)
+        {
+            Console.WriteLine("Error");
+
+        }
+        else
+        {
+            Console.WriteLine(ans);
+
+        }
+
+    }
+    static double condition(double n)
+    {
+        if (n * n <= 4)
+        {
+            return 2;
+        }
+        else if (n * n > 4)
+        {
+            return 1 + condition(n - (4 * Math.Abs(n) / n));
+        }
+        return -999;
+    }
+}
