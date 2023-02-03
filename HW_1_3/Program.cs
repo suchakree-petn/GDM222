198d972f28ed8e4d6f94b798e7226844dea85ce5
suchakree-petn <suchakree.petn@kmutt.ac.th>
2023-02-03 17:47:25 +0700

    Add HW_1_3

---
+++ HW_1_3/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        double n = double.Parse(Console.ReadLine());
+        double ans = condition(n);
+        if (ans == -999)
+        {
+            Console.WriteLine("Error");
+
+        }
+        else
+        {
+            Console.WriteLine(ans);
+
+        }
+
+    }
+    static double condition(double n)
+    {
+        if (n > 1)
+        {
+            return condition(n - 2);
+        }
+        else if (n <= -1)
+        {
+            return condition(n + 2);
+        }
+        else if (n > -1 && n <= 0)
+        {
+            return Math.Sqrt(1 - (n * n));
+        }
+        else if (n > 0 && n <= 1)
+        {
+            return -1*Math.Sqrt(1 - (n * n));
+        }
+        return -999;
+    }
+}
