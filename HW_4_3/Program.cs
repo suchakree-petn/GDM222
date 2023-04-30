36ecc30156a113185bbf95fa2b9a9e0a48b6e3c6
suchakree-petn <suchakree.petn@kmutt.ac.th>
2023-04-30 21:46:29 +0700

    Add HW_4_3

---
+++ HW_4_3/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.Write("Max Node: ");
+        int maxNodeCount = int.Parse(Console.ReadLine());
+        Graph graph = new Graph(maxNodeCount);
+        while (true)
+        {
+            Console.Write("Source: ");
+            int sourceNode = int.Parse(Console.ReadLine());
+            if (sourceNode + 1 > maxNodeCount || sourceNode < 0)
+            {
+                break;
+            }
+            Console.Write("Destination: ");
+            int destNode = int.Parse(Console.ReadLine());
+            if (destNode + 1 > maxNodeCount || destNode < 0)
+            {
+                break;
+            }
+            graph.AddEdge(sourceNode, destNode);
+        }
+        if (graph.IsReachable(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())))
+        {
+            Console.WriteLine("Reachable");
+        }
+        else
+        {
+            Console.WriteLine("Unreachable");
+        }
+    }
+}
